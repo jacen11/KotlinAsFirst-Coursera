3400af48a90a33c76bac8344c245e7b0c3ab7883
jacen11 <Jacen19931@gmail.com>
2019-03-29 01:01:30 +0300

    lesson2.task2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -1,7 +1,9 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task2
 
 import lesson1.task1.sqr
+import lesson6.task1.dateStrToDigit
 
 /**
  * Пример
@@ -35,7 +37,17 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    if (year % 4 == 0 && month == 2 && (year % 400 == 0 || year % 100 != 0)) {
+        return 29
+    }
+    when (month) {
+        4, 6, 9, 11 -> return 30
+        1, 3, 5, 7, 8, 10, 12 -> return 31
+        2 -> return 28
+    }
+    return 1
+}
 
 /**
  * Средняя
@@ -45,7 +57,8 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean = sqr(Math.pow(x2 - x1, 2.0) + Math.pow(y2 - y1, 2.0)) + r1 <= r2
+
 
 /**
  * Средняя
